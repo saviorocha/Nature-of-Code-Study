8ba15aa36c6dfde36c8a5720bced5a5235f595dd
saviorocha <44814299+saviorocha@users.noreply.github.com>
2020-06-10 16:01:25 -0300

    Solved Paint Splatter Exercise

--- Introduction/Gaussian-Paint-Splatter/index.js
+++ Introduction/Gaussian-Paint-Splatter/index.js
@@ -1,7 +1,27 @@
 function setup() {
-	createCanvas(640, 240);
+	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-	background(255);
+	let stdDeviation = 60;
+	let mean = width / 2;
+	let randGauss = randomGaussian(mean, stdDeviation);
+	
+	let rand = (Math.random() * randGauss + randGauss) / (Math.random() * (mean / 10));
+	let cond = true;
+	
+	noStroke();
+	fill(randColor(100, 10), randColor(200, 20), randColor(15, 50));
+
+	if (cond) {
+		ellipse(randGauss, rand, rand / 30, rand / 30);
+		cond = false;
+	} else {
+		ellipse(rand, randGauss, rand / 30, rand / 30);
+		cond = true;
+	}
+}
+
+function randColor(mean, std) {
+	return Math.floor(randomGaussian(mean, std));
 }
